6fd31508f5889bb6f16f26eff8a5a6f061e4f0fb
Виктория <nika.kr@mail.ru>
2016-10-03 20:52:18 +0300

    19-20 homework

---
+++ js 19-20/src/script/js.js
@@ -0,0 +1,53 @@
+"use strict";
+$(function() {
+
+
+// Carousel  
+  $('.jcarousel').jcarousel({
+    wrap: 'circular'
+  });
+  $('.jcarousel-pagination')
+     .on('jcarouselpagination:active', 'a', function(e) {
+         $(this).addClass('active');
+     })
+     .on('jcarouselpagination:inactive', 'a', function(e) {
+         $(this).removeClass('active');
+     })
+     .on('click', function(e) {
+         e.preventDefault();
+     })
+     .jcarouselPagination({
+         perPage: 1,
+         item: function(page) {
+             return '<a href="#"></a>';
+         }});
+   
+
+  $.getJSON('data.json', function(data) {
+    var dataSkills = _.map(data, 'skills');
+    dataSkills = _.flatten(dataSkills);
+    dataSkills = _.uniq(dataSkills);
+    dataSkills = _.orderBy(dataSkills);
+    console.log('Массив скиллов (поле skills) всех людей, отсортированный по алфавиту, без повторяющихся скиллов:');
+    console.log(dataSkills);
+    console.log(' ');
+
+    var dataName = _.map(data, function(x) {return _.pick(x, 'name','friends')});
+    dataName = _.sortBy(dataName, function (x) {return x.friends.length});
+    dataName = _.map(dataName, 'name');
+    console.log(' Массив имен (поле name) людей, отсортированных в зависимости от количества их друзей (friends):');
+    console.log(dataName);
+    console.log(' ');
+
+    var dataFriends = _.map(data, 'friends');
+    dataFriends = _.flatten(dataFriends);
+    dataFriends = _.map(dataFriends, 'name');
+    dataFriends = _.uniq(dataFriends);
+    console.log('Массив всех друзей всех пользователей, без повторяющихся людей:');
+    console.log(dataFriends);
+    console.log(' ');
+
+  });
+  
+});
+
